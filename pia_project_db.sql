dffd1894eff24e112d89df2518169b604aa686ba
Ognjen Babovic <ognjen.t.babovic@gmail.com>
2017-02-15 19:16:29 +0100

    Add db initialization file

--- pia_project_db.sql
+++ pia_project_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8001
--- Generation Time: Feb 15, 2017 at 06:22 PM
+-- Generation Time: Feb 15, 2017 at 07:12 PM
 -- Server version: 5.6.33
 -- PHP Version: 7.0.12
 
@@ -42,8 +42,8 @@ CREATE TABLE `festival` (
 
 CREATE TABLE `festival_comment` (
   `id` int(11) NOT NULL,
-  `id_festival` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `festival` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
   `text` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -55,8 +55,8 @@ CREATE TABLE `festival_comment` (
 
 CREATE TABLE `festival_performer` (
   `id` int(11) NOT NULL,
-  `id_festival` int(11) NOT NULL,
-  `id_performer` int(11) NOT NULL,
+  `festival` int(11) NOT NULL,
+  `performer` int(11) NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -69,8 +69,8 @@ CREATE TABLE `festival_performer` (
 
 CREATE TABLE `festival_rating` (
   `id` int(11) NOT NULL,
-  `id_festival` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `festival` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
   `rating` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -92,8 +92,9 @@ CREATE TABLE `performer` (
 --
 
 CREATE TABLE `reservation` (
-  `id_user` int(11) NOT NULL,
-  `id_ticket` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `ticket` int(11) NOT NULL,
   `status` varchar(255) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -108,7 +109,7 @@ CREATE TABLE `social_network` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `link` varchar(255) NOT NULL,
-  `id_festival` int(11) NOT NULL
+  `festival` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -121,7 +122,7 @@ CREATE TABLE `ticket` (
   `id` int(11) NOT NULL,
   `type` varchar(255) NOT NULL,
   `date` date NOT NULL,
-  `id_festival` int(11) NOT NULL
+  `festival` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -138,8 +139,9 @@ CREATE TABLE `user` (
   `surname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
-  `id_role` int(11) NOT NULL,
-  `num_res_attempts` int(11) NOT NULL
+  `role` int(11) NOT NULL,
+  `num_res_attempts` int(11) NOT NULL,
+  `approved` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -168,25 +170,25 @@ ALTER TABLE `festival`
 --
 ALTER TABLE `festival_comment`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_festivals` (`id_festival`),
-  ADD KEY `id_users` (`id_user`);
+  ADD KEY `id_festivals` (`festival`),
+  ADD KEY `id_users` (`user`);
 
 --
 -- Indexes for table `festival_performer`
 --
 ALTER TABLE `festival_performer`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_festivals` (`id_festival`),
-  ADD KEY `id_performers` (`id_performer`);
+  ADD KEY `id_festivals` (`festival`),
+  ADD KEY `id_performers` (`performer`);
 
 --
 -- Indexes for table `festival_rating`
 --
 ALTER TABLE `festival_rating`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_festival_2` (`id_festival`,`id_user`),
-  ADD KEY `id_festival` (`id_festival`),
-  ADD KEY `id_user` (`id_user`);
+  ADD UNIQUE KEY `id_festival_2` (`festival`,`user`),
+  ADD KEY `id_festival` (`festival`),
+  ADD KEY `id_user` (`user`);
 
 --
 -- Indexes for table `performer`
@@ -198,22 +200,24 @@ ALTER TABLE `performer`
 -- Indexes for table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`id_user`,`id_ticket`),
-  ADD KEY `id_tickets` (`id_ticket`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_2` (`user`,`ticket`),
+  ADD KEY `user` (`user`),
+  ADD KEY `ticket` (`ticket`);
 
 --
 -- Indexes for table `social_network`
 --
 ALTER TABLE `social_network`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_festivals` (`id_festival`);
+  ADD KEY `id_festivals` (`festival`);
 
 --
 -- Indexes for table `ticket`
 --
 ALTER TABLE `ticket`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_festivals` (`id_festival`);
+  ADD KEY `id_festivals` (`festival`);
 
 --
 -- Indexes for table `user`
@@ -221,7 +225,7 @@ ALTER TABLE `ticket`
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
-  ADD KEY `role` (`id_role`);
+  ADD KEY `role` (`role`);
 
 --
 -- Indexes for table `user_role`
@@ -259,6 +263,11 @@ ALTER TABLE `festival_rating`
 ALTER TABLE `performer`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `social_network`
 --
 ALTER TABLE `social_network`
@@ -286,44 +295,44 @@ ALTER TABLE `user_role`
 -- Constraints for table `festival_comment`
 --
 ALTER TABLE `festival_comment`
-  ADD CONSTRAINT `festival_comment_ibfk_1` FOREIGN KEY (`id_festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `festival_comment_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `festival_comment_ibfk_1` FOREIGN KEY (`festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `festival_comment_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `festival_performer`
 --
 ALTER TABLE `festival_performer`
-  ADD CONSTRAINT `festival_performer_ibfk_1` FOREIGN KEY (`id_performer`) REFERENCES `performer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `festival_performer_ibfk_2` FOREIGN KEY (`id_festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `festival_performer_ibfk_1` FOREIGN KEY (`performer`) REFERENCES `performer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `festival_performer_ibfk_2` FOREIGN KEY (`festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `festival_rating`
 --
 ALTER TABLE `festival_rating`
-  ADD CONSTRAINT `festival_rating_ibfk_1` FOREIGN KEY (`id_festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `festival_rating_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `festival_rating_ibfk_1` FOREIGN KEY (`festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `festival_rating_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`id_ticket`) REFERENCES `ticket` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`ticket`) REFERENCES `ticket` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `social_network`
 --
 ALTER TABLE `social_network`
-  ADD CONSTRAINT `social_network_ibfk_1` FOREIGN KEY (`id_festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `social_network_ibfk_1` FOREIGN KEY (`festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `ticket`
 --
 ALTER TABLE `ticket`
-  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`festival`) REFERENCES `festival` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `user_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role`) REFERENCES `user_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
