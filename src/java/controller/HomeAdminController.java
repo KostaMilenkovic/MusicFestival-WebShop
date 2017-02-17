082b9245721b322bf2edeb96eb2d98a9184fe019
Sikimic <sikimic94@gmail.com>
2017-02-17 16:54:30 +0100

    Add homeAdminController

---
+++ src/java/controller/HomeAdminController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import model.User;
+import db.DB;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import model.Festival;
+import model.FestivalRating;
+
+@ManagedBean(name = "homeAdminController")
+@ViewScoped
+public class HomeAdminController implements Serializable{
+    
+    
+}
